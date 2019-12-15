1c94d9e56c701eb2f655e2e692e64744f4e44671
qin yu peng <qin.batista@outlook.com>
2019-12-15 21:59:10 +0800

    更新docker文件

--- 15_photo_manager/photo_manager.py
+++ 15_photo_manager/photo_manager.py
@@ -1,10 +1,12 @@
 #%%
+print("start...")
 from datetime import datetime
 import os
-
+import time
+import shutil
 def get_time():
 	current_time = datetime.now().strftime("%Y-%m")
-	current_folder = os.path.abspath(os.path.dirname(__file__))+"/"+current_time
+	current_folder = "./"+current_time
 	if not os.path.exists(current_folder):os.makedirs(current_folder)
 	return current_time, current_folder
 
@@ -23,16 +25,19 @@ def analysis_folder():
 			folder_list.append(file_name_list)
 	return folder_list,file_list
 # %%
-import time
-import shutil
 def loop_command():
+	print(os.listdir('.'))
+	print("negivate...")
+	os.chdir('/photo_manager')
+	print(os.listdir('.'))
 	while True:
 		current_time,current_folder = get_time()
 		folder_list,file_list = analysis_folder()
 		for file_name in file_list:
+			print("file_name:"+file_name)
+			print("current_folder/file_name:"+current_folder+"/"+file_name)
 			shutil.move(file_name, current_folder+"/"+file_name)
 		time.sleep(5)
-loop_command()
-
-
+if __name__ == "__main__":
+	loop_command()
 # %%
