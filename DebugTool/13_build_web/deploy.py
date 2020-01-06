c4e2d3287aeb3b06323a450bedc5d49bb51b7a06
qin yu peng <qin.batista@outlook.com>
2020-01-06 16:43:50 +0800

    同步部分配置文件

---
+++ DebugTool/13_build_web/deploy.py
@@ -0,0 +1,17 @@
+import sys, os
+import socket
+import threading
+from time import ctime
+
+def DeployServer(web_path):
+	os.chdir(os.getcwd())
+	os.system("apt-get update")
+	os.system("apt-get -y install apache2")
+	os.system("cp -rf "+web_path+" /var/www/")
+	folder_name = web_path[web_path.rfind('/')+1:]
+	os.system("mv /var/www/"+folder_name+" /var/www/website")
+	os.system("cp 000-default.conf /etc/apache2/sites-available/000-default.conf")
+	os.system("/etc/init.d/apache2 restart")
+
+if __name__ == '__main__':
+	DeployServer(input('input your web path:'))
