0a89f6a576aaeea0cf248ac797f8fdbe65444c3e
qin yu peng <qin.batista@outlook.com>
2020-02-02 15:36:57 +0800

    更新基础安装

--- basic_deploy.py
+++ basic_deploy.py
@@ -8,6 +8,11 @@ def DeployServer():
 	os.system("cp ./DebugTool/04_speedtest/speedtest.py /usr/local/bin/")
 	os.system("cp ./DebugTool/11_iptest/iptest.py /usr/local/bin/")
 	os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
+	os.system("python3  ./DebugTool/02_pip3/get-pip.py")
+	os.system("pip3 install youtube-dl")
+	os.system("pip3 install instagram-scraper")
+	os.system("curl -fsSL https://get.docker.com -o get-docker.sh")
+	os.system("sh get-docker.sh")
 
 
 if __name__ == '__main__':
