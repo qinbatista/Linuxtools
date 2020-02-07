415178fd896b54472a3121f589ae125d0eb81a0d
qin yu peng <qin.batista@outlook.com>
2020-02-07 18:57:08 +0800

    更新新的deploy文件

--- basic_deploy.py
+++ basic_deploy.py
@@ -9,6 +9,7 @@ def DeployServer():
 	os.system("cp ./DebugTool/11_iptest/iptest.py /usr/local/bin/")
 	os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
 	os.system("python3  ./DebugTool/02_pip3/get-pip.py")
+	os.system("python3  ./DebugTool/13_build_web/deploy.py")
 	os.system("pip3 install youtube-dl")
 	os.system("pip3 install instagram-scraper")
 	os.system("curl -fsSL https://get.docker.com -o get-docker.sh")
