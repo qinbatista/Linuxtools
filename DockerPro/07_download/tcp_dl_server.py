527c863064afd770ad110594b30c8b2601f67a9f
qin yu peng <qin.batista@outlook.com>
2020-02-14 18:14:53 +0800

    根系

--- DockerPro/07_download/tcp_dl_server.py
+++ DockerPro/07_download/tcp_dl_server.py
@@ -91,7 +91,6 @@ if __name__ == "__main__":
 
 	current_milli_time = lambda: int(round(time.time() * 1000))
 	task_id = current_milli_time()
-	os.system("ssh-keygen")
 	os.system("cat  ~/.ssh/id_rsa.pub")
 	os.system('rsync -avz --progress -e "ssh -p 10022" ~/download root@cqhome.qinbatista.com:~/download/')
 	qs = QinServer()
