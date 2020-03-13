1b532ee26868aa8e21590370330047793dae8c49
qin yu peng <qin.batista@outlook.com>
2020-03-13 15:35:51 +0800

    更新

--- DockerPro/07_download/tcp_dl_server.py
+++ DockerPro/07_download/tcp_dl_server.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
 	current_milli_time = lambda: int(round(time.time() * 1000))
 	task_id = current_milli_time()
 	os.system("cat  ~/.ssh/id_rsa.pub")
-	os.system('rsync -avz --progress -e "ssh -o stricthostkeychecking=no -p 10022" ~/download root@cqhome.qinbatista.com:~/download/')
+	os.system('rsync -avz --progress -e "ssh -o stricthostkeychecking=no -p 10022" ~/download root@cqhome.qinbatista.com:~/')
 	qs = QinServer()
 	qs.start_server()
 
