0143a271ee72132329b1afa14fad590a4606d2d7
qin yu peng <qin.batista@outlook.com>
2020-05-24 23:35:58 +0800

    更新

--- DockerPro/07_download/tcp_dl_server.py
+++ DockerPro/07_download/tcp_dl_server.py
@@ -80,7 +80,7 @@ class QinServer:
 
 	async def __mission_manager(self,message,type,proxy):
 		if proxy!='': proxy = 'proxychains'
-		if   type == "youtube-dl": self.__thread_download(f'{proxy} {type} {message}')
+		if   type == "youtube-dl": self.__thread_download(f'{proxy} {type} -f bestvideo+bestaudio {message}')
 		elif type == "wget": self.__thread_download(f'{proxy} {type} {message}')
 		elif type == "instagram-scraper": self.__thread_download(f'{proxy} {type} {message}')
 		elif type == "aria2c": self.__thread_download(f'{proxy} {type} {message}')
