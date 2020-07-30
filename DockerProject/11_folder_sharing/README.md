eced191796f5f5f08effa7f24a51f879f8480a01
qin yu peng <qin.batista@outlook.com>
2020-07-30 14:58:35 +0800

    更新服务器

--- DockerProject/11_folder_sharing/README.md
+++ DockerProject/11_folder_sharing/README.md
@@ -1,12 +1,12 @@
 #### RUN
 > 22：ssh，18184：下载工具 80:网站
 ```
-docker run -itv /root/OperationLives:/root/OperationLives -v /root/redeemsystem:/root/redeemsystem -p 9989:9989  qinbatista/redeemsystem
+docker run -itv /root/download:/root/download_folder  -p 9998:9998  qinbatista/redeemsystem
 ```
 
 > build
 
 ```
-docker build -t qinbatista/redeemsystem .
+docker build -t qinbatista/FTPCreater .
 ```
 
--- DockerProject/11_folder_sharing/ftp_server.py
+++ DockerProject/11_folder_sharing/ftp_server.py
@@ -2,14 +2,20 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 import os
+import random
 class FtpManager:
-	def __init__(self, worlds = []):
+	def __init__(self, port):
 		self.__reedem_codes_file = 'redeem'
-		self.__download_folder = "/Users/batista/Desktop/" #~/download_folder/
+		self.__download_folder = "~/download_folder/"#"/Users/batista/Desktop/" #~/download_folder/
+		self.__readme = "README.txt" #~/download_folder/
+		self.__port = 9998
+		self.__access_URL = "ftp://office.singmaan.com:"+str(self.__port) #~/download_folder/
 
 	def	__list_folder(self, _path):
 		List = []
 		for i in os.listdir(_path):
+			if i.find(".DS_Store")!=-1 or i.find(".")!=-1:
+				continue
 			List.append(i)
 		return List
 
@@ -29,26 +35,27 @@ class FtpManager:
 	def create_file(self,_path,_context):
 		with open(_path,mode='w',encoding="utf8") as file_context:
 			file_context.write(_context)
+
 	def start_server(self):
 		folder_name = self.__list_folder(self.__download_folder)
-		print("folder_name="+folder_name)
+		print("folder_name="+str(folder_name))
 		#实例化DummyAuthorizer来创建ftp用户
 		authorizer = DummyAuthorizer()
 		for my_name in folder_name:
 		# 参数：用户名，密码，目录，权限
-			os.mkdir(self.__download_folder+"/"+my_name)
 			my_password = self.generate_verification_code()
-			print(f"create usersname:{my_name},password:{my_password},path_name:{self.__download_folder+my_name}")
+			print(f"URL:		{self.__access_URL}\nPathName:	{self.__download_folder+my_name}\nUserName:	{my_name}\nPassword:	{my_password}\n")
+			self.create_file(self.__download_folder+"/"+my_name+"/"+self.__readme, f"URL:		{self.__access_URL}\nPathName:	{self.__download_folder+my_name}\nUserName:	{my_name}\nPassword:	{my_password}\n")
 			authorizer.add_user(my_name, my_password, self.__download_folder+my_name, perm='elradfmwMT')
 		# 匿名登录
 		# authorizer.add_anonymous('/home/nobody')
 		handler = FTPHandler
 		handler.authorizer = authorizer
 		# 参数：IP，端口，handler
-		server = FTPServer(('', 9988), handler)
+		server = FTPServer(('', self.__port), handler)
 		server.serve_forever()
 
 
 if __name__ == '__main__':
-	my_ftp = FtpManager()
+	my_ftp = FtpManager(9988)
 	my_ftp.start_server()
