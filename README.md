830c8ea86cb713b48e431931b9acbe560237e2c3
qin yu peng <qin.batista@outlook.com>
2021-04-27 19:18:34 +0800

    更新speedtest的文件

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 execute command
 
 ```
-apt-get update && apt-get -y install python3 git screen  iptraf && git clone https://qinbatista:qinyupeng1@bitbucket.org/qinbatista/linuxtools.git && cd linuxtools && python3 basic_deploy.py && cd ~/linuxtools/Qinbatista_DDNS
+apt-get update && apt-get -y install python3 git screen  iptraf && git clone https://qinbatista:qinyupeng1@bitbucket.org/qinbatista/linuxtools.git && cd linuxtools && python3 basic_deploy.py && cd ~/linuxtools/QinbatistaCom_DDNS
 ```
 
--- basic_deploy.py
+++ basic_deploy.py
@@ -5,12 +5,13 @@ from time import ctime
 
 def DeployServer():
 	os.chdir(os.getcwd())
-	os.system("cp ./DebugTool/04_speedtest/speedtest.py /usr/local/bin/")
 	os.system("cp ./DebugTool/11_iptest/iptest.py /usr/local/bin/")
 	os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
 	os.system("python3  ./DebugTool/02_pip3/get-pip.py")
 	os.system("curl -fsSL https://get.docker.com -o get-docker.sh")
 	os.system("sh get-docker.sh")
+	os.system("wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py")
+	os.system("cp ./speedtest.py /usr/local/bin/")
 	os.system("docker run -itd -p 7000-7030:7000-7030  qinbatista/ssr")
 	os.system("docker run -itdv /root/download:/root/download -v /root/deliveried:/root/deliveried -p 10022:22 -p 18184:18184  qinbatista/download")
 
