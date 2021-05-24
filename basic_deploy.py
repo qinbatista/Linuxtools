e2a67bd2ecadd034917bde9fd47ad9dc358dfc64
qin.batista <qin.batista@outlook.com>
2021-05-24 18:54:16 +0800

    update

--- basic_deploy.py
+++ basic_deploy.py
@@ -4,10 +4,22 @@ import threading
 from time import ctime
 
 def DeployServer():
+	
 	os.chdir(os.getcwd())
+	#open root
+	os.system("sudo -s")
+	#install basic tool
+	os.system("apt-get update && apt-get -y install python3 git screen  iptraf")
+	#open bbr
+	os.system('echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf')
+	os.system('echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf')
+	os.system('sysctl -p')
+	os.system('sysctl net.ipv4.tcp_available_congestion_control')
+	#check if bbr is opening: lsmod | grep bbr
+
 	os.system("cp ./DebugTool/11_iptest/iptest.py /usr/local/bin/")
 	os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
-	os.system("python3  ./DebugTool/02_pip3/get-pip.py")
+	os.system("python ./DebugTool/02_pip3/get-pip.py")
 	os.system("curl -fsSL https://get.docker.com -o get-docker.sh")
 	os.system("sh get-docker.sh")
 	os.system("wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py")
