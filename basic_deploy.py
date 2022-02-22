f8dc001a1cbc5d1bcb9d43ef3669be9212e9b946
qin yu peng <qin.batista@outlook.com>
2022-02-22 15:14:00 -0800

    ss

--- basic_deploy.py
+++ basic_deploy.py
@@ -15,17 +15,20 @@ def DeployServer():
 	os.system('sysctl -p')
 	os.system('sysctl net.ipv4.tcp_available_congestion_control')
 	#check if bbr is opening: lsmod | grep bbr
-
 	os.system("cp ./DebugTool/11_iptest/iptest.py /usr/local/bin/")
-	os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
-	os.system("python ./DebugTool/02_pip3/get-pip.py")
+	# os.system("bash ./DebugTool/12_install_python3/install_python-x.sh")
+	# os.system("python ./DebugTool/02_pip3/get-pip.py")
 	os.system("curl -fsSL https://test.docker.com -o test-docker.sh")
 	os.system("sh test-docker.sh")
 	os.system("apt-get install docker-ce docker-ce-cli containerd.io")
 	os.system("wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py")
+	os.system("docker run -itdv /root/download:/download -p 10005:10005  qinbatista/download")
 	os.system("cp ./speedtest.py /usr/local/bin/")
 	os.system("docker run -itd -p 7000-7030:7000-7030  qinbatista/ssr")
-	os.system("docker run -itdv /root/download:/download -p 10005:10005  qinbatista/download")
+	os.system("iptables -A INPUT -p udp --dport 12345 -j ACCEPT")
+	os.system("iptables -A INPUT -p udp --dport 53 -j ACCEPT")
+	os.system("iptables-save")
+	
 
 if __name__ == '__main__':
 	DeployServer()
